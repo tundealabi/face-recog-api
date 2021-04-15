fa4ad6e3dc27f450dfdb05a8259ed9067cea747a
Tunde <tundealabi4780@gmail.com>
2021-04-15 14:53:30 +0100

    add home route

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json({limit:'50mb'}));
 app.use("/user",userSigninRoute);
 app.use("/face-recog", require("./routes/image-route"));
 
+app.get("/",(req,res,next) => res.json({message: "Welcome home"}))
 
 app.use((err,req,res,next)=>{
     console.log(err.message);
