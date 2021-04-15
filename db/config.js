9568e5a76eb7cfe5f897d1086358177969641c29
Tunde <tundealabi4780@gmail.com>
2021-04-15 14:44:37 +0100

    fix database connection bug

--- db/config.js
+++ db/config.js
@@ -2,10 +2,11 @@ const config = require("config");
 
 let configObject = {
     client: 'pg',
-    connection: "postgres://kheexhfsjotrrq:74299c55de2b551904a443a50868f811bd840f9575f893386f4367051cb28ed5@ec2-107-22-83-3.compute-1.amazonaws.com:5432/ddrqcl3pnasidc",
-	ssl: {
-    rejectUnauthorized: false
-  }
+    connection: {
+		connectionString: "postgres://kheexhfsjotrrq:74299c55de2b551904a443a50868f811bd840f9575f893386f4367051cb28ed5@ec2-107-22-83-3.compute-1.amazonaws.com:5432/ddrqcl3pnasidc",
+		ssl: true
+	}
+	
 };
 
 // if(process.env.NODE_ENV === "production"){
