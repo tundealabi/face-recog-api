51b5d5f3a7e601cc5e4f1062d1793e835fa29969
Tunde <tundealabi4780@gmail.com>
2021-04-15 23:58:36 +0100

    reinstall pg module

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const helmet = require("helmet");
 const userSigninRoute = require("./routes/user-route");
 
 const app = express();
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 app.use(express.json({limit:'50mb'}));
 app.use("/user",userSigninRoute);
 app.use("/face-recog", require("./routes/image-route"));
